fc5c465879e69b1436e9bd5ae599de7f8bc7ee38
Truong Quy <nguyentruongquy75@gmail.com>
2022-01-26 11:41:50 +0700

    Add user agent

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ link.forEach(async (item) => {
         headers: {
           Referer: "https://www.ssphim.net/",
           "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
         },
       },
       item.group
