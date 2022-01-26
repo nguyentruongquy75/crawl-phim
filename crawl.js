5ab21de38cc8e0b20cba275e62f417bdd98e91b5
Truong Quy <nguyentruongquy75@gmail.com>
2022-01-26 11:50:28 +0700

    Fix

--- crawl.js
+++ crawl.js
@@ -6,7 +6,7 @@ const Ep = require("./models/Ep");
 const baseUrl = "https://www.ssphim.net";
 
 const crawl = async (link, group) => {
-  request(link, (err, res, html) => {
+  request(link).then((html) => {
     const $ = cheerio.load(html);
 
     $(".item").each((index, ele) => {
@@ -19,91 +19,80 @@ const crawl = async (link, group) => {
       const name = $(ele).find(".item-block-title").text();
       const category = $(ele).find(".info-title-link").text();
 
-      request(
-        {
-          url,
+      request({
+        url,
+        headers: {
+          Referer: "https://www.ssphim.net/",
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
+        },
+      }).then(async (html) => {
+        const $ = cheerio.load(html);
+        const watchUrl = baseUrl + $(".button_xemphim").attr().href;
+        const shortDesc = $(".header-short-description p").html();
+        const review = $("#review .w-richtext").html();
+        const trailer = $("#trailer iframe").attr()?.src;
+
+        const film = new Film({
+          name,
+          category,
+          image,
+          shortDesc,
+          review,
+          trailer,
+          ep: [],
+          group,
+        });
+
+        await request({
+          url: watchUrl,
           headers: {
             Referer: "https://www.ssphim.net/",
             "User-Agent":
-              "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
           },
-        },
-        async (err, res, html) => {
+        }).then(async (html) => {
           const $ = cheerio.load(html);
-          const watchUrl = baseUrl + $(".button_xemphim").attr().href;
-          const shortDesc = $(".header-short-description p").html();
-          const review = $("#review .w-richtext").html();
-          const trailer = $("#trailer iframe").attr()?.src;
+          const isMultipleEp = $(".episodes-list .collection-item").length
+            ? true
+            : false;
 
-          const film = new Film({
-            name,
-            category,
-            image,
-            shortDesc,
-            review,
-            trailer,
-            ep: [],
-            group,
-          });
+          if (isMultipleEp) {
+            $(".episodes-list .collection-item").each(async (index, ele) => {
+              const epName = $(ele).text();
+              const epLink = baseUrl + $(ele).find("a").attr().href;
 
-          await request(
-            {
-              url: watchUrl,
-              headers: {
-                Referer: "https://www.ssphim.net/",
-                "User-Agent":
-                  "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
-              },
-            },
-            async (err, res, html) => {
-              const $ = cheerio.load(html);
-              const isMultipleEp = $(".episodes-list .collection-item").length
-                ? true
-                : false;
-
-              if (isMultipleEp) {
-                $(".episodes-list .collection-item").each(
-                  async (index, ele) => {
-                    const epName = $(ele).text();
-                    const epLink = baseUrl + $(ele).find("a").attr().href;
-
-                    request(
-                      {
-                        url: epLink,
-                        headers: {
-                          Referer: "https://www.ssphim.net/",
-                          "User-Agent":
-                            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
-                        },
-                      },
-                      (err, res, html) => {
-                        const $ = cheerio.load(html);
-                        const video = $(".w-iframe iframe").attr()?.src;
-                        const ep = new Ep({
-                          epName,
-                          video,
-                        });
-                        ep.save();
-                        film.ep.push(ep._id);
-                      }
-                    );
-                  }
-                );
-              } else {
+              request({
+                url: epLink,
+                headers: {
+                  Referer: "https://www.ssphim.net/",
+                  "User-Agent":
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
+                },
+              }).then((html) => {
+                const $ = cheerio.load(html);
                 const video = $(".w-iframe iframe").attr()?.src;
                 const ep = new Ep({
-                  epName: "Full",
+                  epName,
                   video,
                 });
                 ep.save();
                 film.ep.push(ep._id);
-              }
-            }
-          );
+              });
+            });
+          } else {
+            const video = $(".w-iframe iframe").attr()?.src;
+            const ep = new Ep({
+              epName: "Full",
+              video,
+            });
+            ep.save();
+            film.ep.push(ep._id);
+          }
+        });
 
-          film.save();
-        }
-      );
+        film.save();
+      });
     });
   });
 };
